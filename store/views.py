d54aba474bc7014f78c64585ea20198d4a591fcb
Thato <thatomphahlele57@gmail.com>
2026-03-12 22:18:47 +0200

    Added a product detail page: Add URL, create the view/template and made products clickable

--- store/views.py
+++ store/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def product_list(request):
-    products = Product.objects.all()  # get all products from database
+    products = Product.objects.all()
     return render(request, 'store/product_list.html', {'products': products})
+
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'store/product_detail.html', {'product': product})
