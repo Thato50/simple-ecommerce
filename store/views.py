27b43b259dcc72a23d8a8ba4864f388fee3ed9d3
Thato <thatomphahlele57@gmail.com>
2026-03-12 18:16:52 +0200

    Project Setup: configured Django models, admin panel,database tables, product view, templates, URL routing and media handling for product images

--- store/views.py
+++ store/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.all()  # get all products from database
+    return render(request, 'store/product_list.html', {'products': products})
